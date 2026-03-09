bcb3aa7f2f754d487e6458819a9e1c81e2e96d6a
Fil Heinz O. Re La Torre <filheinz27@gmail.com>
2026-03-09 15:01:47 +0800

    Fix sitemap error

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://filheinzrelatorre.com';
+    const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://www.filheinzrelatorre.com';
 
     return [
         {
